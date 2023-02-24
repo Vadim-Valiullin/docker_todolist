d62aee6067e9ec5160171717c7c967a968a97da1
Vadim-Valiullin <vadimvr-ufa@yandex.ru>
2023-02-24 16:40:33 +0500

    default authentication

--- todolist/settings.py
+++ todolist/settings.py
@@ -30,7 +30,9 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    'django.contrib.staticfiles'
+    'rest_framework',
+    # 'social_django',
     'core',
 
 ]
