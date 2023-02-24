b30fc186a56bf82dbe7c42e63dc59bce7853a691
Vadim-Valiullin <vadimvr-ufa@yandex.ru>
2023-02-24 16:40:33 +0500

    default authentication

--- core/views.py
+++ core/views.py
@@ -1,3 +1,47 @@
-from django.shortcuts import render
+from django.contrib.auth import login, logout
+from rest_framework import generics, status, permissions
+from rest_framework.response import Response
 
-# Create your views here.
+from core.models import User
+from core.serializers import CreateUserSerializer, LoginSerializer, ProfileSerializer, UpdatePasswordSerializer
+
+
+class SignupView(generics.CreateAPIView):
+    serializer_class = CreateUserSerializer
+
+
+# class LoginView(generics.GenericAPIView):
+#     serializer_class = LoginSerializer
+#
+#     def post(self, request, *args, **kwargs):
+#         serializer = self.get_serializer(data=request.data)
+#         serializer.is_valid(raise_exception=True)
+#         login(request=request, user=serializer.save())
+#         return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+class LoginView(generics.CreateAPIView):
+    serializer_class = LoginSerializer
+
+    def perform_create(self, serializer):
+        login(request=self.request, user=serializer.save())
+
+
+class ProfileView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = ProfileSerializer
+    queryset = User.objects.all()
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self) -> User:
+        return self.request.user
+
+    def delete(self, request, *args, **kwargs):
+        logout(request)
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+class UpdatePasswordView(generics.UpdateAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+    serializer_class = UpdatePasswordSerializer
+
+    def get_object(self):
+        return self.request.user
+
